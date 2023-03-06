58c7c0cda07dd324f303b7a801b6f6049caa7886
Alec <31748545+burturt@users.noreply.github.com>
2023-03-05 17:30:56 -0800

    Database (#16)
    add db

---
+++ database/index.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "data/database.sqlite",
+});
+
+reminders = sequelize.define("reminders", {
+  discordID: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  reminderID: {
+    type: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  remindTime: {
+    type: Sequelize.DATE,
+  },
+  data: {
+    type: Sequelize.TEXT,
+  },
+});
+
+reminders.sync();
+
+module.exports.reminders = reminders;
