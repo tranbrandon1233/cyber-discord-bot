6afd080efa6a051c9fedf05950f3e60f5a6f8969
Brandon Tran <tranbrandon1233@gmail.com>
2023-03-12 18:00:51 -0700

    Prettify files

--- commands/role-request.js
+++ commands/role-request.js
@@ -1,31 +1,39 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder} = require('discord.js');
-
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  SlashCommandBuilder,
+} = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName("request-role")
     .setDescription("Request a role."),
-	async execute(interaction) {
-		const row = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId('role-request--cyber-academy')
-					.setLabel('Cyber Academy')
-					.setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-					.setCustomId('role-request--special-topics')
-					.setLabel('Special Topics')
-					.setStyle(ButtonStyle.Primary),
-			);
+  async execute(interaction) {
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("role-request--cyber-academy")
+        .setLabel("Cyber Academy")
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setCustomId("role-request--special-topics")
+        .setLabel("Special Topics")
+        .setStyle(ButtonStyle.Primary)
+    );
 
-		await interaction.reply({ content: 'Choose a role: ', components: [row] });
-	},
-	async onMessageInteraction(interaction) {
-		if (interaction.customId === 'role-request--cyber-academy') {
-			await interaction.reply({ content: 'You have been given the Cyber Academy role.', ephemeral: true });
-		}
-		else if (interaction.customId === 'role-request--special-topics') {
-			await interaction.reply({ content: 'You have been given the Special Topics role.', ephemeral: true });
-		}
-	}
+    await interaction.reply({ content: "Choose a role: ", components: [row] });
+  },
+  async onMessageInteraction(interaction) {
+    if (interaction.customId === "role-request--cyber-academy") {
+      await interaction.reply({
+        content: "You have been given the Cyber Academy role.",
+        ephemeral: true,
+      });
+    } else if (interaction.customId === "role-request--special-topics") {
+      await interaction.reply({
+        content: "You have been given the Special Topics role.",
+        ephemeral: true,
+      });
+    }
+  },
 };
